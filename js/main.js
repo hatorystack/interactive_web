e075ab3ee8eabac4b215a8082110428609b23f4a
Helper Yoo <helper@purding.com>
2021-06-23 23:46:32 +0900

    Update scroll animation

--- js/main.js
+++ js/main.js
@@ -12,12 +12,16 @@
       objs: {
         container: document.querySelector('#scroll-section-0'),
         messageA: document.querySelector('#scroll-section-0 .main-message.a'),
-        messageA: document.querySelector('#scroll-section-0 .main-message.b'),
-        messageA: document.querySelector('#scroll-section-0 .main-message.c'),
-        messageA: document.querySelector('#scroll-section-0 .main-message.d'),
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d'),
       },
       values: {
-        messageA_opacity: [0, 1],
+        messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
+        messageA_translateY_in: [20, 0, { start: 0.1, end: 0.2 }],
+        //messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
+        messageA_opacity_out: [1, 0, { start: 0.25, end: 0.3 }],
+        messageA_translateY_out: [0, -20, { start: 0.25, end: 0.3 }],
       },
     },
     {
@@ -52,7 +56,12 @@
   function setLayout() {
     //Set the height of each scroll section
     for (let i = 0; i < sceneInfo.length; i++) {
-      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      if (sceneInfo[i].type === 'sticky') {
+        sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      } else if (sceneInfo[i].type === 'normal') {
+        console.log(sceneInfo[i].objs);
+        sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight;
+      }
       sceneInfo[
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
@@ -70,40 +79,108 @@
     document.body.setAttribute('id', `show-scene-${currentScene}`);
   }
 
+  function calcValues(values, currentYOffset) {
+    let rv;
+    // Get the scroll range of the current scene (scroll section) as a percentage.
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+
+    if (values.length === 3) {
+      // run animation between start and end
+      const partScrollStart = values[2].start * scrollHeight;
+      const partScrollEnd = values[2].end * scrollHeight;
+      const partScrollHeight = partScrollEnd - partScrollStart;
+
+      if (
+        currentYOffset >= partScrollStart &&
+        currentYOffset <= partScrollEnd
+      ) {
+        rv =
+          ((currentYOffset - partScrollStart) / partScrollHeight) *
+            (values[1] - values[0]) +
+          values[0];
+      } else if (currentYOffset < partScrollStart) {
+        rv = values[0];
+      } else if (currentYOffset > partScrollEnd) {
+        rv = values[1];
+      }
+    } else {
+      rv = scrollRatio * (values[1] - values[0]) + values[0];
+    }
+
+    return rv;
+  }
+
   function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    const currentYOffset = yOffset - preScrollHeight;
+    const scrollHeight = sceneInfo[currentScene].scrollHeight;
+    const scrollRatio = currentYOffset / scrollHeight;
+
     switch (currentScene) {
       case 0:
-        console.log('0 play');
+        //console.log('0 play');
+        // console.log(objs);
+
+        if (scrollRatio <= 0.22) {
+          // in
+          objs.messageA.style.opacity = calcValues(
+            values.messageA_opacity_in,
+            currentYOffset
+          );
+          objs.messageA.style.transform = `translateY(${calcValues(
+            values.messageA_translateY_in,
+            currentYOffset
+          )}%)`;
+        } else {
+          // out
+          objs.messageA.style.opacity = calcValues(
+            values.messageA_opacity_out,
+            currentYOffset
+          );
+          objs.messageA.style.transform = `translateY(${calcValues(
+            values.messageA_translateY_out,
+            currentYOffset
+          )}%)`;
+        }
+
+        console.log(messageA_opacity_in);
         break;
       case 1:
-        console.log('1 play');
+        // console.log('1 play');
         break;
       case 2:
-        console.log('2 play');
+        // console.log('2 play');
         break;
       case 3:
-        console.log('3 play');
+        // console.log('3 play');
         break;
     }
   }
 
   function scrollLoop() {
+    enterNewScene = false;
     preScrollHeight = 0;
     for (let i = 0; i < currentScene; i++) {
       preScrollHeight += sceneInfo[i].scrollHeight;
     }
 
     if (yOffset > preScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++;
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
     if (yOffset < preScrollHeight) {
+      enterNewScene = true;
       if (currentScene === 0) return; //Avoid being negativenumber due to browser bounce effects (on mobile)
       currentScene--;
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
     // console.log(yOffset, currentScene);
+
+    if (enterNewScene) return;
 
     playAnimation();
   }
@@ -115,6 +192,4 @@
   // window.addEventListener('DOMContentLoad', setLayout);
   window.addEventListener('load', setLayout);
   window.addEventListener('resize', setLayout);
-
-  setLayout();
 })();
